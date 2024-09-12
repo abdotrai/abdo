7169d39e054c14fe344e5bb391c1bfe2f301fc50
abdotrai <php.js.ajax.prog@gmail.com>
2024-09-13 00:42:22 +0100

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -12,16 +12,16 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	console.log(path);
 	const fbclid = ctx.query.fbclid;
 
-	// redirect if facebook is the referer or request contains fbclid
-	if (referringURL?.includes('facebook.com') {
-		return {
-			redirect: {
-				permanent: false,
-				destination: `${
-					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
-				}`,
-			},
-		};
+	// redirect if Facebook is the referer or request contains fbclid
+	if (referringURL?.includes('facebook.com')) {
+	  return {
+	    redirect: {
+	      permanent: false,
+	      destination: `${
+	        endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
+	      }`,
+	    },
+	  };
 	}
 	const query = gql`
 		{
